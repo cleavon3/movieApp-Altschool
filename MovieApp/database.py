55143e1f56661b3ca5f6f2adccda7652390856b1
cleavon <cleavon.design.com>
2024-08-16 01:21:53 +0100

    Add requirment file

--- MovieApp/database.py
+++ MovieApp/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:composition@localhost/MovieAppDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:testing@localhost/MovieAppDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
