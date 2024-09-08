e7366bd5c12d0204ecd30115fc5cab37f7014960
cleavon <cleavon.design.com>
2024-09-08 20:54:52 +0100

    update

--- MovieApp/database.py
+++ MovieApp/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:composition@localhost/MovieAppDatabase'
+#SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:composition@localhost/MovieAppDatabase'
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://user:composition@localhost:10000/MovieAppDatabase"
+
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
